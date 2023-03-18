0545f2da7393aa1d74d3b45dcd49b6e6129b07cc
DanielBar2145658 <98904765+DanielBar2145658@users.noreply.github.com>
2023-03-17 23:32:57 -0400

    Lab7

--- Topdown2DGame/Assets/Scripts/Player/PlayerMovement.cs
+++ Topdown2DGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,7 +7,10 @@ public class PlayerMovement : MonoBehaviour
 
     public PlayerStats stats;
     public Rigidbody2D rb2D;
-    [SerializeField]
+    public Transform shootPoint;
+    public GameObject bullet;
+    
+   [SerializeField]
     Vector2 move;
 
     // Start is called before the first frame update
@@ -28,17 +31,20 @@ public class PlayerMovement : MonoBehaviour
     }
 
 
-    void Input() 
+    void Input()
     {
         move.x = UnityEngine.Input.GetAxisRaw("Horizontal");
         move.y = UnityEngine.Input.GetAxisRaw("Vertical");
 
-        if (UnityEngine.Input.GetKeyDown(KeyCode.F)) 
+        if (UnityEngine.Input.GetKeyDown(KeyCode.F))
         {
             stats.SpeedBuff(4);
         }
-        
-        
+
+        if (UnityEngine.Input.GetMouseButtonDown(0)) 
+        {
+            StartCoroutine(Shoot());
+        }
     
     }
     void Movement() 
@@ -48,4 +54,11 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
+    IEnumerator Shoot() 
+    {
+        yield return new WaitForSeconds(0.01f);
+
+        Instantiate(bullet, shootPoint.transform.position, Quaternion.identity);
+
+    }
 }
